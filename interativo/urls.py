3956635b9ea76d3ef2bbe2a060ecb73f5b54b69f
rustybreaker <rustybreaker.likeaboss@gmail.com>
2025-05-06 21:28:46 -0300

    Atualização

--- interativo/urls.py
+++ interativo/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
     path('quiz/', views.quiz, name='quiz'),
-    path('final/', views.final, name='final'),
+    path('galeria/', views.galeria, name='galeria'),
+    path('cartinha/', views.cartinha, name='cartinha'),
 ]
--- interativo/views.py
+++ interativo/views.py
@@ -6,5 +6,12 @@ def index(request):
 def quiz(request):
     return render(request, 'quiz.html')
 
-def final(request):
-    return render(request, 'final.html')
+def menu(request):
+    return render(request, 'menu.html')
+
+def galeria(request):
+    return render(request, 'galeria.html')  # ainda vamos criar esse template
+
+def cartinha(request):
+    return render(request, 'cartinha.html')
+
